28fe66c4f1a42bbf0278f105b99d2fc1fa82a633
Eric Born <eric.born85@gmail.com>
2019-11-19 15:56:47 -0800

    Update main.py
    Notes for the projects scope

--- main.py
+++ main.py
@@ -3,5 +3,19 @@
 Created on Tue Nov 19 15:39:56 2019
 
 @author: Eric
+
+youtube video that inspired this project
+https://www.youtube.com/watch?v=oAHbLRjF0vo
 """
 
+# setup function that takes number of seats per row and total rows
+# optional method used to assign seats to passengers, can be random if not specified
+# outputs time to fill the seats
+
+# develop methods of assignment
+# random, back to front, front to back, one side then other back to front, 
+# window then middle then isle
+
+# random needs to be included in the time delay between reaching seat and sitting down
+# next passenger cannot move forward until previous is seated
+# add time delay if passengers seat is blocked by other passengers already seated
