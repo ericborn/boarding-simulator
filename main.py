c6041c91feec813dfa75dc9b67565b97f5646a99
Eric Born <eric.born85@gmail.com>
2019-11-27 19:41:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Tue Nov 19 15:39:56 2019
 youtube video that inspired this project
 https://www.youtube.com/watch?v=oAHbLRjF0vo
 """
+from random import random
 
 # setup function that takes number of seats per row and total rows
 # optional method used to assign seats to passengers, can be random if not specified
@@ -19,3 +20,22 @@ https://www.youtube.com/watch?v=oAHbLRjF0vo
 # random needs to be included in the time delay between reaching seat and sitting down
 # next passenger cannot move forward until previous is seated
 # add time delay if passengers seat is blocked by other passengers already seated
+
+
+def simulate(width, row):
+    self.width = width
+    self.row = row
+    self. time = time
+    return('Total time: ' + time)
+
+# row is the total depth of the plane in number of seats
+# width is number of seats per side of the plane
+# total is (width + width) * row
+row = 30
+width = 3
+total = (width + width) * row
+
+# seat assignemts is a dictionary containing sequential numbers from 1..x
+# where x represents the highest numbered passenger on the plane.
+# assignents
+seat_assignments = {}
