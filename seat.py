16040fe9fde2d1c79e14aff136967264e9711512
Eric Born <eric.born85@gmail.com>
2019-12-03 09:36:01 -0800

    Update seat.py
    added size

--- seat.py
+++ seat.py
@@ -9,16 +9,15 @@ Defines the seat starting size, x/y position and color
 class Seat:
     # init method is run when seat classes are initialized
     # self allows you to set variables that the class always starts with
-    def __init__(self, color):
-        
-        # x coordinate
-        self.x = 50
-        
-        # y coordinate
-        self.y = 50
-        
-        # used for both width and height since we want a square
-        self.size = 10
+    def __init__(self, color, size):
+    #def __init__(self, color, x_bound, y_bound):
+
+        # define the x/y boundaries
+#        self.x_bound = x_bound
+#        self.y_bound = y_bound
         
         # starting color
         self.color = color
+        
+        # used for both width and height since we want a square
+        self.size = size
